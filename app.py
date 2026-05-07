2010fcb432c459352d0c5373bcf775436e7f31d1
Diego Regis <roisadi@gmail.com>
2026-05-07 15:59:07 -0300

    Criando tabelas PostgreSQL

--- app.py
+++ app.py
@@ -833,4 +833,7 @@ def cobrar():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    with app.app_context():
+    db.create_all()
+
+app.run(host='0.0.0.0', port=5000, debug=True)
