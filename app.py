35ac93419cc4ef4dcb935f39cb1b7799f6bf2d18
Diego Regis <roisadi@gmail.com>
2026-05-20 16:21:10 -0300

    Criando tabelas automaticamente

--- app.py
+++ app.py
@@ -840,9 +840,8 @@ def cobrar():
         dados=dados
     )
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-
-    with app.app_context():
-        db.create_all()
-
     app.run(host='0.0.0.0', port=5000, debug=True)
