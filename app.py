12dadc1f8e9946b363581acca0bd2d487b97b872
Diego Regis <roisadi@gmail.com>
2026-05-30 11:25:36 -0300

    Ajustando dados da academia na carteirinha

--- app.py
+++ app.py
@@ -1716,8 +1716,8 @@ def carteirinha_aluno(id):
 
     contato_x = int(largura * 0.555)
     endereco_y = int(altura * 0.750)
-    whatsapp_y_academia = int(altura * 0.760)
-    instagram_y = int(altura * 0.770)
+    whatsapp_y_academia = int(altura * 0.780)
+    instagram_y = int(altura * 0.800)
 
     draw.text(
         (contato_x, endereco_y),
